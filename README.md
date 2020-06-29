ff3640c41794a39ff11c97aee53e703eefc14fe3
abdoulsalamally <abdelsalami2016@gmail.com>
2020-06-29 09:36:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Currency Converter
+
+cu-conve is a simple currency Conversion mobile application which is developed by using ionic framework. Ionic framework is a cross platform application development framework widely used by many organizations.
+
+![021Artboard 1@4x](https://user-images.githubusercontent.com/25591638/57977907-81193b00-7a1f-11e9-9bd7-bb5a2b50a49d.png)
+
+
+## Learning outcomes
+- Data binding
+- Event binding
+- HTTPClient  
+- JSON Mapping
+- Asynchronous method
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { WheelSelector } from '@ionic-native/wheel-selector';
+import { HttpModule } from "@angular/http";
+import { HttpClientModule } from '@angular/common/http';
+import { CurrencyService } from "../pages/service/cu-service";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    CurrencyService,
+    WheelSelector,{provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
